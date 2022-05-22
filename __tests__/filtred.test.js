74c0d975b6d216531e39a9b06c27923f3e4c07d7
TheKr1d <epifanov.vv.spb@gmail.com>
2022-05-22 11:54:17 +0300

    изменено:      .eslintrc.yml новый файл:    .npmrc изменено:      __tests__/filtred.test.js изменено:      src/index.js

--- __tests__/filtred.test.js
+++ __tests__/filtred.test.js
@@ -18,12 +18,16 @@ const requiredRange3 = [200, null];
 const expectedRange1 = [
   { name: 'Courses in England', prices: [0, 100] },
   { name: 'Courses in Italy', prices: [100, 200] },
+  { name: 'Courses in Russia', prices: [null, 400] },
+  { name: 'Courses in China', prices: [50, 250] },
   { name: 'Courses in USA', prices: [200, null] },
+  { name: 'Courses in Kazakhstan', prices: [56, 324] },
   { name: 'Courses in France', prices: [null, null] },
 ];
 const expectedRange2 = [
   { name: 'Courses in England', prices: [0, 100] },
   { name: 'Courses in Italy', prices: [100, 200] },
+  { name: 'Courses in Russia', prices: [null, 400] },
   { name: 'Courses in China', prices: [50, 250] },
   { name: 'Courses in USA', prices: [200, null] },
   { name: 'Courses in Kazakhstan', prices: [56, 324] },
@@ -40,7 +44,7 @@ const expectedRange3 = [
 ];
 
 test('Testing filtred', () => {
-  expect(filtred(courses, requiredRange1)).toBe(expectedRange1),
-  expect(filtred(courses, requiredRange2)).toBe(expectedRange2),
-  expect(filtred(courses, requiredRange3)).toBe(expectedRange3);
+  expect(filtred(courses, requiredRange1)).toStrictEqual(expectedRange1),
+  expect(filtred(courses, requiredRange2)).toStrictEqual(expectedRange2),
+  expect(filtred(courses, requiredRange3)).toStrictEqual(expectedRange3)
 });
--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
 #!/usr/bin/env node
+const getNormalisedNumber = (item) => {
+    const min = item[0] === null ? 0 : item[0];
+    const max = item[1] === null ? Infinity : item[1];
+    return [min, max];
+};
 
-const filtred = () => console.log('Hello');
+const filtred = (arrCurses, options) => {
+    const [minOptions, maxOptions] = getNormalisedNumber(options);
+    const result = [];
+    arrCurses.forEach((item) => {
+        const [minCurses, maxCurses] = getNormalisedNumber(item.prices);
+        if (maxOptions >= minCurses && minOptions <= maxCurses) {
+            result.push(item);
+        }
+    })
+    return result;
+}
 export default filtred;
